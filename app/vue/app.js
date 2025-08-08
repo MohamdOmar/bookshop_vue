afb210deef90c77209878ad4b0aabc70fdc1fdd3
Daniel Hutzel <daniel.hutzel@sap.com>
2025-08-08 06:53:43 +0200

    Using Fetch API instead of axios in Vue app

--- app/vue/app.js
+++ app/vue/app.js
@@ -1,46 +1,44 @@
-import { createApp, ref, reactive } from 'vue'
+import { createApp, ref, reactive } from './vue.js'
 import cds from './cap.js'
 
-const { GET, POST } = await cds.connect.to ('/rest/catalog')
+const { GET, POST } = cds.connect.to ('/rest/catalog')
+const $ = s => document.querySelector (s)
+
 createApp ({ setup() {
 
-  const $ = sel => document.querySelector(sel)
-  const books = ref([]), book = ref(undefined)
+  const books = ref([])
+  const book = ref(undefined)
   const order = reactive({ quantity:1 })
   const message = reactive({ reset() { this.succeeded = this.failed = undefined } })
+  const stars = [ '☆☆☆☆☆', '★☆☆☆☆', '★★☆☆☆', '★★★☆☆', '★★★★☆', '★★★★★' ]
 
-  return {
-    books, book, order, message, Ratings: { get undefined() { return this [Math.round(Math.random()*5)] },
-      0: '☆☆☆☆☆',
-      1: '★☆☆☆☆',
-      2: '★★☆☆☆',
-      3: '★★★☆☆',
-      4: '★★★★☆',
-      5: '★★★★★',
-    },
 
-    async fetch (pattern, $search = pattern ? `&$search=${pattern}` : '') {
-      const { data } = await GET `/ListOfBooks?$expand=genre($select=name),currency($select=symbol)${$search}`
-      books.value = data
+  return { books, book, order, message, stars,
+
+    async fetch ($) {
+      books.value = await GET `ListOfBooks?$expand=genre($select=name),currency($select=symbol)${
+        $ ? `&$search=${$}` : ''
+      }`
     },
 
     async inspect (index) { message.reset()
-      const { ID } = book.value = books.value [index]
-      const { data } = await GET `/Books/${ID}?$select=descr,stock`
-      Object.assign (book.value, data)
-      Object.assign (order, { book: ID, quantity: 1 })
-      setTimeout (()=> $('form > input').focus(), 111)
+      let { ID } = book.value = books.value [index]
+      Object.assign (book.value, await GET `Books/${ID}?$select=descr,stock`)
+      Object.assign (order, { book: ID, quantity: 1 }) // reset order for selected book
+      setTimeout (()=> $('form > input').focus(), 11) // focus input field after rendering
     },
 
     async submitOrder() { message.reset()
       try {
-        const { data } = await POST (`/submitOrder`, order)
+        let { stock } = await POST (`submitOrder`, order)
+        book.value.stock = stock
         message.succeeded = `Successfully ordered ${order.quantity} item(s).`
-        book.value.stock = data.stock
       } catch (e) {
-        message.failed = e.response.data.error ? e.response.data.error.message : e.response.data
+        message.failed = e.message
+        throw e
       }
     },
   }
 
 }}) .mount('#app') .fetch() // initially fill list of books
+$('#app > input').focus()
--- app/vue/cap.js
+++ app/vue/cap.js
@@ -1,65 +1,75 @@
-
+/**
+ * USAGE @example
+ * import cds from 'cap.js'; const { GET, POST } = cds.connect.to ('/rest/catalog')
+ * import fetch from 'cap.js'; const { GET, POST } = fetch.for ('/rest/catalog')
+ */
 export default {
-  connect: {
-    to (service, options) {
-      if (!service.startsWith('/')) service = '/' + service
-      if (!service.endsWith('/')) service = service + '/'
-      return new RemoteService (service, options)
-    }
+  connect: { to: (base, defaults) => new Fetcher (base, defaults) },
+  for: (base, defaults) => new Fetcher (base, defaults),
+}
+
+/** A CAP Service lookalike for HTTP clients */
+export class Fetcher {
+
+  // These are convenience methods that still return standard fetch Response objects
+  options (..._) { return this.fetch ('OPTIONS', ..._) }
+  head (..._)    { return this.fetch ('HEAD', ..._) }
+  get (..._)     { return this.fetch ('GET', ..._) }
+  put (..._)     { return this.fetch ('PUT', ..._) }
+  post (..._)    { return this.fetch ('POST', ..._) }
+  patch (..._)   { return this.fetch ('PATCH', ..._) }
+  delete (..._)  { return this.fetch ('DELETE', ..._) }
+
+  // These are convenience methods that return unwrapped response data
+  read(..._)   { return this.get(..._)   .then (_respond) }
+  create(..._) { return this.post(..._)  .then (_respond) }
+  insert(..._) { return this.post(..._)  .then (_respond) }
+  upsert(..._) { return this.put(..._)   .then (_respond) }
+  update(..._) { return this.patch(..._) .then (_respond) }
+
+  // These are bound convenience methods that return unwrapped response data
+  get GET()    { return (..._) => this.get(..._)    .then (_respond) }
+  get PUT()    { return (..._) => this.put(..._)    .then (_respond) }
+  get POST()   { return (..._) => this.post(..._)   .then (_respond) }
+  get PATCH()  { return (..._) => this.patch(..._)  .then (_respond) }
+  get DELETE() { return (..._) => this.delete(..._) .then (_respond) }
+
+  constructor (base, defaults) {
+    this.base = base[0] === '/' ? base : '/' + base
+    this.defaults = { ...defaults, headers: {
+      'content-type': 'application/json',
+      ...defaults?.headers
+    }}
+  }
+
+  /** This is the core method to send requests using Fetch API */
+  async fetch (method, url, d, o) {
+    if (url.raw) [url,d,o] = [String.raw (url,d,o)] // support tagged template literals
+    const o2 = !d ? { ...o } : d?.headers || d?.body ? { ...d } : { ...o, body: _serialized (d) }
+    const d2 = this.defaults; for (let k in d2)
+      o2[k] = typeof d2[k] === 'object' ? { ...d2[k], ...o2[k] } : o2[k] ?? d2[k]
+
+    // send the request, and return the response, if successful
+    const res = await fetch (this.base + (url[0] === '/' ? url : '/' + url), { ...o2, method })
+    if (res.ok) return res
+
+    // if failed, construct and throw an error
+    const { data, error:e = data.error } = await res.json(); e.message ??= `HTTP ${res.status}: ${res.statusText}`
+    throw Object.assign (new Error, e)
   }
 }
 
-class RemoteService {
+/** A helper function to unwrap response data */
+const _respond = r => { switch (r.headers.get('content-type')?.split(';')[0]) {
+  case 'application/json': return _unwrap (r.json())
+  case 'application/xml': return _unwrap (r.text())
+  case 'text/plain': return _unwrap (r.text())
+  case 'text/html': return _unwrap (r.text())
+  default: return r // leaving it to the caller to handle
+}}
 
-  constructor (service) { this.service = service }
-  get GET() { return this.get.bind(this) }
-  get PUT() { return this.put.bind(this) }
-  get POST() { return this.post.bind(this) }
-  get PATCH() { return this.patch.bind(this) }
-  get DELETE() { return this.delete.bind(this) }
+/** Helper function to unwrap response data from OData, GraphQL, REST, and HCQL servers */
+const _unwrap = async d => (d = await d).value?.data || d.value || d.data || d
 
-  async options (...args) { return axios.options(...this.args(...args)) }
-  async head (...args) { return axios.head(...this.args(...args)) }
-  async get (...args) { return axios.get(...this.args(...args)) }
-  async put (...args) { return axios.put(...this.args(...args)) }
-  async post (...args) { return axios.post(...this.args(...args)) }
-  async patch (...args) { return axios.patch(...this.args(...args)) }
-  async delete (...args) { return axios.delete(...this.args(...args)) }
-
-  async list (...args) { return axios.get(...this.args(...args)) }
-  async read (...args) { return axios.get(...this.args(...args)) }
-  async create (...args) { return axios.post(...this.args(...args)) }
-  async insert (...args) { return axios.post(...this.args(...args)) }
-  async upsert (...args) { return axios.put(...this.args(...args)) }
-  async update (...args) { return axios.patch(...this.args(...args)) }
-
-  args (url, ...args) {
-    if (url.raw) return [ this.service + String.raw(url,...args) ]
-    else return [ this.service + url, ...args ]
-  }
-}
-
-
-import axios from 'axios'
-
-axios.interceptors.request.use(csrfToken)
-function csrfToken (request) {
-  if (request.method === 'head' || request.method === 'get') return request
-  if ('csrfToken' in document) {
-    request.headers['x-csrf-token'] = document.csrfToken
-    return request
-  }
-  return fetchToken().then(token => {
-    document.csrfToken = token
-    request.headers['x-csrf-token'] = document.csrfToken
-    return request
-  }).catch(() => {
-    document.csrfToken = null // set mark to not try again
-    return request
-  })
-
-  function fetchToken() {
-    return axios.get('/', { headers: { 'x-csrf-token': 'fetch' } })
-    .then(res => res.headers['x-csrf-token'])
-  }
-}
+/** Helper function to check if a body is an object that needs serialization */
+const _serialized = x => x && typeof x === 'object' && !(x instanceof FormData) ? JSON.stringify(x) : x /* global FormData */
